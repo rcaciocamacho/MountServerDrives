9a5b71fb86f649a92769bf6a402b495fa2d145d6
Rubén Cacio <rcaciocamacho@gmail.com>
2024-03-12 16:22:26 +0100

    Fist commit.

---
+++ mount_server_drives.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Cargar variables desde el archivo .env y convertirlas a minúsculas
+if [ -f ".env" ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo "Archivo .env no encontrado."
+    exit 1
+fi
+
+# Definir arrays para almacenar las variables
+remote_names=()
+remote_paths=()
+local_mount_points=()
+
+# Leer las variables y almacenarlas en los arrays
+i=1
+while true; do
+    remote_name_var="remote_name_$i"
+    remote_path_var="remote_path_$i"
+    local_mount_point_var="local_mount_point_$i"
+
+    if [ -z "${!remote_name_var}" ] || [ -z "${!remote_path_var}" ] || [ -z "${!local_mount_point_var}" ]; then
+        break
+    fi
+
+    remote_names+=("${!remote_name_var}")
+    remote_paths+=("${!remote_path_var}")
+    local_mount_points+=("${!local_mount_point_var}")
+
+    ((i++))
+done
+
+# Montar directorios remotos
+for ((i = 0; i < ${#remote_names[@]}; i++)); do
+    # Montar el directorio remoto usando rclone mount
+    umount "${local_mount_points[i]}"
+    sshfs "${remote_names[i]}:${remote_paths[i]}" "${local_mount_points[i]}" &
+    
+    # Esperar un breve periodo para asegurar que el montaje se complete
+    sleep 5
+done
+
+# Verificar si los montajes fueron exitosos
+if [ $? -eq 0 ]; then
+    echo "Unidades remotas montadas correctamente."
+    notify-send "Unidades remotas montadas correctamente."
+else
+    echo "Hubo un error al montar las unidades remotas."
+    notify-send "Hubo un error al montar las unidades remotas."
+fi
---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Directorio de instalación
+install_dir="$HOME/.config/remoteMount"
+
+# Crear carpeta remoteMount en el directorio .config si no existe
+if [ ! -d "$install_dir" ]; then
+    mkdir -p "$install_dir"
+    echo "Carpeta remoteMount creada en $install_dir."
+fi
+
+# Copiar todos los archivos del directorio actual a la carpeta de instalación
+cp -r ./* "$install_dir/"
+
+# Obtener la ruta del script principal
+script_path="$install_dir/mount_server_drives.sh"
+
+# Comprobar si .zshrc existe
+zshrc_path="$HOME/.zshrc"
+if [ -f "$zshrc_path" ]; then
+    # Comprobar si el alias ya está definido en .zshrc
+    if grep -q "alias remoteMount" "$zshrc_path"; then
+        echo "El alias 'remoteMount' ya está definido en $zshrc_path."
+    else
+        # Añadir alias al archivo .zshrc
+        echo "alias remoteMount='$script_path'" >> "$zshrc_path"
+        echo "Alias 'remoteMount' añadido a $zshrc_path."
+        echo "Por favor, reinicia tu terminal o ejecuta 'source $zshrc_path' para aplicar los cambios."
+    fi
+else
+    echo "No se encontró el archivo .zshrc en $HOME. No se pudo crear el alias."
+fi
